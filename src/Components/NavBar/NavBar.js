edc7daf78a0b2965eb37032d68e4a3ee2ce26f7a
Steven-Klavins <stevenklavins94@gmail.com>
2020-06-10 14:09:10 +0100

    Link/Styling: added link for twitter and icon

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -2,6 +2,7 @@ import React from 'react'
 import '../NavBar/NavBar.css'
 import Background from '../Background/Background'
 import codewarsLogo from '../../images/codewars-logo.png'
+import twitterLogo from '../../images/twitter-logo.png'
 
 const navbar = () => {
   return (
@@ -26,9 +27,22 @@ const navbar = () => {
         </li>
       </ul>
       <div className="logobox">
-        <a href="https://www.codewars.com/users/Steven-Klavins" target="_blank">
+        <a
+          className="logos"
+          href="https://www.codewars.com/users/Steven-Klavins"
+          target="_blank"
+        >
           <img className="logos" src={codewarsLogo} alt="Code Wars Logo" />
           Code Wars
+        </a>
+
+        <a
+          className="logos"
+          href="https://twitter.com/KlavinsSteven"
+          target="_blank"
+        >
+          <img className="logos" src={twitterLogo} alt="Twitter Logo" />
+          Twitter
         </a>
       </div>
       <div>
