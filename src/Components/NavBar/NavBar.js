ee37334a263d8096a64fa11e79da69563a0445e7
Steven-Klavins <stevenklavins94@gmail.com>
2020-06-10 16:58:25 +0100

    Link: added medium link and logo

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -6,6 +6,7 @@ import codewarsLogo from '../../images/codewars-logo.png'
 import twitterLogo from '../../images/twitter-logo.png'
 import linkedinLogo from '../../images/linkedin-logo.png'
 import githublogo from '../../images/github-logo.png'
+import mediumlogo from '../../images/medium-logo.png'
 
 const navbar = () => {
   return (
@@ -44,6 +45,15 @@ const navbar = () => {
             image={linkedinLogo}
             description={'Linkedin logo'}
             title={'Linkedin'}
+          ></Icon>
+        </div>
+
+        <div>
+          <Icon
+            link={'https://medium.com/@stevenklavins94'}
+            image={mediumlogo}
+            description={'Medium Logo'}
+            title={'Medium'}
           ></Icon>
         </div>
 
