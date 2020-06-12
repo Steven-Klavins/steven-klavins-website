64838fc86c491dc1ec5a4c5765505e033aa4a474
Steven-Klavins <stevenklavins94@gmail.com>
2020-06-12 13:46:19 +0100

    Component: added About component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from './Components/NavBar/NavBar'
-import Home from './Components/Pages/Home/Home'
+import Home from './Pages/Home/Home'
+import About from './Pages/About/About'
 import './App.css'
 
 class App extends React.Component {
---
+++ src/Pages/About/About.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../About/About.css'
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="nameTitle">About</h1>
+      <div className="footerNote">
+        Built and designed by Steven Klavins 2020
+      </div>
+    </div>
+  )
+}
+
+export default About
