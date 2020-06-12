bffa61a44b0c24e475d0e859947607017053a73a
Steven-Klavins <stevenklavins94@gmail.com>
2020-06-12 13:56:13 +0100

    Component: added Projects component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from './Components/NavBar/NavBar'
 import Home from './Pages/Home/Home'
 import About from './Pages/About/About'
 import Skills from './Pages/Skills/Skills'
+import Projects from './Pages/Projects/Projects'
 import './App.css'
 
 class App extends React.Component {
---
+++ src/Pages/Projects/Projects.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Projects/Projects.css'
+
+const Projects = () => {
+  return (
+    <div>
+      <h1 className="nameTitle">Projects</h1>
+      <div className="footerNote">
+        Built and designed by Steven Klavins 2020
+      </div>
+    </div>
+  )
+}
+
+export default Projects
