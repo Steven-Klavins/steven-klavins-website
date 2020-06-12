32b7a1817036eb5546ea8c8d1df0a7b9336b1e1e
Steven-Klavins <stevenklavins94@gmail.com>
2020-06-12 13:38:47 +0100

    Component: added home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
-import Particles from 'react-particles-js'
 import NavBar from './Components/NavBar/NavBar'
-import Background from './Components/Background/Background'
+import Home from './Components/Pages/Home/Home'
 import './App.css'
 
 class App extends React.Component {
@@ -10,10 +9,7 @@ class App extends React.Component {
       <div>
         <div>
           <NavBar />
-        </div>
-        <h1 className="nameTitle">Steven Klavins</h1>
-        <div className="footerNote">
-          Built and designed by Steven Klavins 2020
+          <Home />
         </div>
       </div>
     )
---
+++ src/Components/Pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Home/Home.css'
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="nameTitle">Steven Klavins</h1>
+      <div className="footerNote">
+        Built and designed by Steven Klavins 2020
+      </div>
+    </div>
+  )
+}
+
+export default Home
