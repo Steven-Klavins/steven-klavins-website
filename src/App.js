a627dd19ea04c0de6eb5901dac17724cf3459e93
Steven-Klavins <stevenklavins94@gmail.com>
2020-06-12 14:00:00 +0100

    Component: added Contact component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './Pages/Home/Home'
 import About from './Pages/About/About'
 import Skills from './Pages/Skills/Skills'
 import Projects from './Pages/Projects/Projects'
+import Contact from './Pages/Contact/Contact'
 import './App.css'
 
 class App extends React.Component {
---
+++ src/Pages/Contact/Contact.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import '../Contact/Contact.css'
+
+const Contact = () => {
+  return (
+    <div>
+      <h1 className="nameTitle">Contact</h1>
+      <div className="footerNote">
+        Built and designed by Steven Klavins 2020
+      </div>
+    </div>
+  )
+}
+
+export default Contact
