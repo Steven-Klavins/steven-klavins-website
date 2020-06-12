8a74c4b8a1a1e6ff969b0d51fe0c02206b730d1e
Steven-Klavins <stevenklavins94@gmail.com>
2020-06-13 00:41:46 +0100

    Styling: updated home desciption

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,6 +5,16 @@ const Home = () => {
   return (
     <div>
       <h1 className="nameTitle">Steven Klavins</h1>
+      <div>
+        <h2>Welcome</h2>
+        <p>
+          Thank you for visiting my site! My name is Steven (most people call me
+          Steve) and I'm a full-stack developer with a yearning to grow and
+          learn. I'll just cut to the chase, tech is basically my life. The
+          endless growth and potential tech has to make the world a better place
+          is what gets me out of bed in the morning.
+        </p>
+      </div>
       <div className="footerNote">
         Built and designed by Steven Klavins 2020
       </div>
