533663e316c752c18d6306219e29e7f2d737202e
Steven-Klavins <stevenklavins94@gmail.com>
2020-06-12 23:47:58 +0100

    Styling: fixed nav bar CSS

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -13,74 +13,76 @@ import cvlogo from '../../images/cv-logo.png'
 const navbar = () => {
   return (
     <div>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/projects">Projects</Link>
-        </li>
-        <li>
-          <Link to="/skills">Skills</Link>
-        </li>
-        <li>
-          <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-          <Link to="/about">About</Link>
-        </li>
-      </ul>
-      <div className="logobox">
-        <div>
-          <Icon
-            link={'https://github.com/Steven-Klavins/CV'}
-            image={cvlogo}
-            description={'CV Logo'}
-            title={'CV'}
-          ></Icon>
-        </div>
-        <div>
-          <Icon
-            link={'https://github.com/Steven-Klavins'}
-            image={githublogo}
-            description={'Github Logo'}
-            title={'Github'}
-          ></Icon>
-        </div>
-        <div>
-          <Icon
-            link={'https://www.linkedin.com/in/steven-klavins-90b02a199/'}
-            image={linkedinLogo}
-            description={'Linkedin logo'}
-            title={'Linkedin'}
-          ></Icon>
-        </div>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/skills">Skills</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+        </ul>
+        <div className="logobox">
+          <div>
+            <Icon
+              link={'https://github.com/Steven-Klavins/CV'}
+              image={cvlogo}
+              description={'CV Logo'}
+              title={'CV'}
+            ></Icon>
+          </div>
+          <div>
+            <Icon
+              link={'https://github.com/Steven-Klavins'}
+              image={githublogo}
+              description={'Github Logo'}
+              title={'Github'}
+            ></Icon>
+          </div>
+          <div>
+            <Icon
+              link={'https://www.linkedin.com/in/steven-klavins-90b02a199/'}
+              image={linkedinLogo}
+              description={'Linkedin logo'}
+              title={'Linkedin'}
+            ></Icon>
+          </div>
 
-        <div>
-          <Icon
-            link={'https://medium.com/@stevenklavins94'}
-            image={mediumlogo}
-            description={'Medium Logo'}
-            title={'Medium'}
-          ></Icon>
-        </div>
+          <div>
+            <Icon
+              link={'https://medium.com/@stevenklavins94'}
+              image={mediumlogo}
+              description={'Medium Logo'}
+              title={'Medium'}
+            ></Icon>
+          </div>
 
-        <div>
-          <Icon
-            link={'https://www.codewars.com/users/Steven-Klavins'}
-            image={codewarsLogo}
-            description={'Code Wars Logo'}
-            title={'Codewars'}
-          ></Icon>
-        </div>
+          <div>
+            <Icon
+              link={'https://www.codewars.com/users/Steven-Klavins'}
+              image={codewarsLogo}
+              description={'Code Wars Logo'}
+              title={'Codewars'}
+            ></Icon>
+          </div>
 
-        <div>
-          <Icon
-            link={'https://twitter.com/KlavinsSteven'}
-            image={twitterLogo}
-            description={'Twitter Logo'}
-            title={'Twitter'}
-          ></Icon>
+          <div>
+            <Icon
+              link={'https://twitter.com/KlavinsSteven'}
+              image={twitterLogo}
+              description={'Twitter Logo'}
+              title={'Twitter'}
+            ></Icon>
+          </div>
         </div>
       </div>
       <div>
