15a882a13540c73d6772d0be9384374f647114c3
Steven <stevenklavins94@gmail.com>
2020-06-16 14:44:50 +0100

    feat: about page has animation

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import '../About/About.css'
 import aboutPhoto from '../../images/about-photo.jpg'
+import { motion } from 'framer-motion'
 
 function About() {
   return (
     <div>
-      <h1 className="nameTitle">About</h1>
-      <div className="aboutTextbox">
-        <p className="aboutTextbox">Hello and thankyou for visiting my site!</p>
-      </div>
-      <img className="aboutimg" src={aboutPhoto}></img>
+      <motion.div
+        exit={{ opasity: 0 }}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+      >
+        <h1 className="nameTitle">About</h1>
+        <div className="aboutTextbox">
+          <p className="aboutTextbox">
+            Hello and thankyou for visiting my site!
+          </p>
+        </div>
+        <img className="aboutimg" src={aboutPhoto}></img>
+      </motion.div>
       <div className="footerNote">
         Built and designed by Steven Klavins 2020
       </div>
