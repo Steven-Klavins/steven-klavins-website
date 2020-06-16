ef67fca0d49c63b6813abbaafb4977934c53b9b5
Steven <stevenklavins94@gmail.com>
2020-06-16 14:45:31 +0100

    feat: home page has animation

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,23 +1,32 @@
 import React from 'react'
 import '../Home/Home.css'
+import { motion } from 'framer-motion'
 
 const Home = () => {
   return (
     <div>
-      <h1 className="nameTitle">Steven Klavins</h1>
-      <div>
-        <h2>Welcome</h2>
-        <p>
-          Thank you for visiting my site! My name is Steven (most people call me
-          Steve) and I'm a full-stack developer with a yearning to grow and
-          learn. I'll just cut to the chase, tech is basically my life. The
-          endless growth and potential tech has to make the world a better place
-          is what gets me out of bed in the morning.
-          <br />
-          <br />
-          Have any questions at all feel free to get in touch!
-        </p>
-      </div>
+      <motion.div
+        exit={{ opasity: 0 }}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+      >
+        <div>
+          <h1 className="nameTitle">Steven Klavins</h1>
+          <div>
+            <h2>Welcome</h2>
+            <p>
+              Thank you for visiting my site! My name is Steven (most people
+              call me Steve) and I'm a full-stack developer with a yearning to
+              grow and learn. I'll just cut to the chase, tech is basically my
+              life. The endless growth and potential tech has to make the world
+              a better place is what gets me out of bed in the morning.
+              <br />
+              <br />
+              Have any questions at all feel free to get in touch!
+            </p>
+          </div>
+        </div>
+      </motion.div>
       <div className="footerNote">
         Built and designed by Steven Klavins 2020
       </div>
