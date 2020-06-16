0e13cd6a79223596d96bb6e331d2ea26cead5424
Steven <stevenklavins94@gmail.com>
2020-06-16 14:45:07 +0100

    feat: contact page has animation

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import '../Contact/Contact.css'
+import { motion } from 'framer-motion'
 
 const Contact = () => {
   return (
     <div>
-      <h1 className="nameTitle">Contact</h1>
-      <div className="footerNote">
-        Built and designed by Steven Klavins 2020
-      </div>
+      <motion.div
+        exit={{ opasity: 0 }}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+      >
+        <h1 className="nameTitle">Contact</h1>
+        <div className="footerNote">
+          Built and designed by Steven Klavins 2020
+        </div>
+      </motion.div>
     </div>
   )
 }
