c77f7f8c4f6bfc19ce5bd3e3db72061b47825e2c
Steven <stevenklavins94@gmail.com>
2020-06-16 14:46:14 +0100

    feat: skills page has animation

--- src/Pages/Skills/Skills.js
+++ src/Pages/Skills/Skills.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import '../Skills/Skills.css'
 import Languages from '../../Components/Languages/Languages'
 import rubyLogo from '../../../src/images/ruby-logo.png'
@@ -9,25 +10,31 @@ import pythonLogo from '../../../src/images/python-logo.png'
 const Skills = () => {
   return (
     <div>
-      <h1 className="nameTitle">Skills</h1>
-      <h2>Languages</h2>
-      <div className="languagesblock">
-        <div>
-          <Languages image={javaLogo} name="Java"></Languages>
+      <motion.div
+        exit={{ opasity: 0 }}
+        animate={{ opacity: 1 }}
+        initial={{ opacity: 0 }}
+      >
+        <h1 className="nameTitle">Skills</h1>
+        <h2>Languages</h2>
+        <div className="languagesblock">
+          <div>
+            <Languages image={javaLogo} name="Java"></Languages>
+          </div>
+          <div>
+            <Languages image={javascriptLogo} name="Javascript"></Languages>
+          </div>
+          <div>
+            <Languages image={rubyLogo} name="Ruby"></Languages>
+          </div>
+          <div>
+            <Languages image={pythonLogo} name="Python"></Languages>
+          </div>
         </div>
-        <div>
-          <Languages image={javascriptLogo} name="Javascript"></Languages>
+        <div className="footerNote">
+          Built and designed by Steven Klavins 2020
         </div>
-        <div>
-          <Languages image={rubyLogo} name="Ruby"></Languages>
-        </div>
-        <div>
-          <Languages image={pythonLogo} name="Python"></Languages>
-        </div>
-      </div>
-      <div className="footerNote">
-        Built and designed by Steven Klavins 2020
-      </div>
+      </motion.div>
     </div>
   )
 }
