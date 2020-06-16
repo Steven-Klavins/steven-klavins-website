ba139d5be2c9ca8e46e3a084939da5b9b40ed248
Steven <stevenklavins94@gmail.com>
2020-06-16 13:59:50 +0100

    Styling: changed styling for about page

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,6 +6,9 @@ function About() {
   return (
     <div>
       <h1 className="nameTitle">About</h1>
+      <div className="aboutTextbox">
+        <p className="aboutTextbox">Hello and thankyou for visiting my site!</p>
+      </div>
       <img className="aboutimg" src={aboutPhoto}></img>
       <div className="footerNote">
         Built and designed by Steven Klavins 2020
