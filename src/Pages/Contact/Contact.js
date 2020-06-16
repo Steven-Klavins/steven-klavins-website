e9ede9743a368dab06cd178a656cbab0a90c314a
Steven <stevenklavins94@gmail.com>
2020-06-16 14:53:16 +0100

    chore: moved footer in projects

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -11,10 +11,10 @@ const Contact = () => {
         initial={{ opacity: 0 }}
       >
         <h1 className="nameTitle">Contact</h1>
-        <div className="footerNote">
-          Built and designed by Steven Klavins 2020
-        </div>
       </motion.div>
+      <div className="footerNote">
+        Built and designed by Steven Klavins 2020
+      </div>
     </div>
   )
 }
