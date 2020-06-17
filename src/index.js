5a69d41173d0600e2bdd7b8f4846b3680a706252
Steven <stevenklavins94@gmail.com>
2020-06-17 21:09:11 +0100

    chore: moved API client

--- src/index.js
+++ src/index.js
@@ -3,10 +3,34 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import './fonts/Antapani-ExtraBold.otf'
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+
+const client = new ApolloClient({
+  uri: 'https://api.github.com/graphql',
+  try: {
+    request: async (operation) => {
+      operation.setContext({
+        headers: {
+          authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+        },
+      })
+    }
+  },
+  catch(error) {
+    console.log(error)
+  },
+})
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 )
+
+
+
+
