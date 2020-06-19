f08f3fe0f40a98abca64070f6877ec05c38ca2f8
Steven <stevenklavins94@gmail.com>
2020-06-19 20:17:01 +0100

    feat: added repo dates

--- src/Components/Repo/Repo.js
+++ src/Components/Repo/Repo.js
@@ -6,6 +6,11 @@ export class Repo extends React.Component {
     return (
       <div className="repBox">
         <span className="pn">{this.props.name}</span>
+        <span className="pn">{this.props.description}</span>
+        <span className="pn">
+          Last updated: {this.props.update.slice(0, 10)}{' '}
+          {this.props.update.slice(11, 16)}
+        </span>
         <a href={this.props.link} target="_blank">
           Link
         </a>
