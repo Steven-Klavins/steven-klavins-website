8c0bc0c87b83b6b7919a32c66aa752ef9f1247f7
Steven <stevenklavins94@gmail.com>
2020-06-19 11:04:00 +0100

    chore: replaced GraphQL with standard API call

--- src/Components/ProjectsTable/ProjectsTable.js
+++ src/Components/ProjectsTable/ProjectsTable.js
@@ -1,39 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../ProjectsTable/ProjectsTable.css'
-import gql from 'graphql-tag'
-import { Query } from 'react-apollo'
-import { from } from 'apollo-boost'
 
-const REPOS = gql`
-  {
-    user(login: "Steven-Klavins") {
-      pinnedItems(last: 5, types: [REPOSITORY, GIST]) {
-        totalCount
-        edges {
-          node {
-            ... on Repository {
-              name
-              primaryLanguage {
-                name
-              }
-              url
-              description
-            }
-          }
-        }
-      }
-    }
-  }
-`
+
+
+
+
 
 const ProjectsTable = () => {
+
+  const [name, setName] = useState('');
+  const [html_url, setHtmlUrl] = useState('');
+  const [description, setDescription] = useState('');
+  const [languages_url, setLanguagesUrl] = useState('');
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/Steven-Klavins/starred")
+      .then(res => res.json())
+      .then(data => {
+        setData(data)
+        console.log(data)
+      })
+  }, [])
+
+  const setData = ({ name, html_url, description, languages_url }) => {
+    setName(name)
+    setHtmlUrl(html_url)
+    setDescription(description)
+    setLanguagesUrl(languages_url)
+
+  }
   return (
     <div className='projectsTable'>
-      <Query query={REPOS} variables={{}}>
-        {({ data, loading, error }) => (
-          error ? <span>Error Loading Github data</span> : <div>{data}</div>
-        )}
-      </Query>
+      <p>Name <span>{name}</span></p>
     </div>
   )
 }
