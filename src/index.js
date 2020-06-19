a287778ebc34179202fd959caef68958261fd353
Steven <stevenklavins94@gmail.com>
2020-06-19 11:03:20 +0100

    chore: removed apollo client

--- src/index.js
+++ src/index.js
@@ -3,30 +3,11 @@ import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
 import './fonts/Antapani-ExtraBold.otf'
-import ApolloClient from 'apollo-boost'
-import { ApolloProvider } from 'react-apollo'
 
-const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
-  try: {
-    request: async (operation) => {
-      operation.setContext({
-        headers: {
-          authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-        },
-      })
-    }
-  },
-  catch(error) {
-    console.log(error)
-  },
-})
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 )
