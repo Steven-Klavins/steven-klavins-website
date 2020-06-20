4732bbc312fcf85f9a03980c9761a468e5e20b41
Steven <stevenklavins94@gmail.com>
2020-06-20 14:00:21 +0100

    chore: reordered repo list

--- src/Components/Repo/Repo.js
+++ src/Components/Repo/Repo.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import '../Repo/Repo.css'
+import gitHubIcon from '../../images/github-logo.png'
 
 export class Repo extends React.Component {
   render() {
     return (
       <div className="repBox">
-        <span className="pn">{this.props.name}</span>
+        <span className="repoTitle">{this.props.name}</span>
+        <div className="gitHubIcon">
+          <img className="gitHubIcon" src={gitHubIcon}></img>
+          <a className="ghLink" href={this.props.link} target="_blank">
+            Link
+          </a>
+        </div>
         <span className="pn">{this.props.description}</span>
+        <br></br>
         <span className="pn">
           Last updated: {this.props.update.slice(0, 10)}{' '}
           {this.props.update.slice(11, 16)}
         </span>
-        <a href={this.props.link} target="_blank">
-          Link
-        </a>
+        <span className="pn">Language: {this.props.language}</span>
       </div>
     )
   }
