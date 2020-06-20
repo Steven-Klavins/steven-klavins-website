01cdc28e110f95db54f196bbaf8114566399c957
Steven <stevenklavins94@gmail.com>
2020-06-20 16:31:13 +0100

    chore: added form component to contact page

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../Contact/Contact.css'
 import { motion } from 'framer-motion'
+import Form from '../../Components/Form/Form'
 
 const Contact = () => {
   return (
@@ -11,6 +12,7 @@ const Contact = () => {
         initial={{ opacity: 0 }}
       >
         <h1 className="nameTitle">Contact</h1>
+        <Form />
       </motion.div>
       <div className="footerNote">
         Built and designed by Steven Klavins 2020
