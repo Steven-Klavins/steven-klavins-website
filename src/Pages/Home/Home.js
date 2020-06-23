6a1ee9fd5b9ffde2ce1ab2523fc8f01f9f2ab1bb
Steven <stevenklavins94@gmail.com>
2020-06-23 17:10:52 +0100

    chore: updated welcome message

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -5,7 +5,7 @@ import { AnimatePresence } from 'framer-motion'
 
 const Home = (isVisible) => {
   return (
-    <div>
+    <div className="fixed">
       <AnimatePresence onExitComplete>
         <motion.div
           key="home"
@@ -19,19 +19,23 @@ const Home = (isVisible) => {
               <h2>Welcome</h2>
               <p>
                 Welcome and thank you for visiting my website! I'm a full-stack
-                developer, Makers Academy graduate, and tech addict. I hold a BA
-                in Digital Arts, perform in a band, and have a love of all
-                things creative. I yearn to make the world a better place
-                through innovative tech, to be frank, its what gets me out of
-                the bed in morning.  
+                developer, Makers Academy graduate, and tech addict. I'm London
+                based however I'm an experienced remote worker and have the
+                flexibility to relocate/travel.<br></br>
+                <br></br>I hold a BA in Digital Arts, perform in a band, and
+                have a love of all things creative. I yearn to make the world a
+                better place through innovative tech, to be frank, its what gets
+                me out of bed in the morning.  
                 <br />
                 <br />
-                Have any questions at all feel please free to get in touch.
+                Want to chat? Feel free to contact me on any of the listed
+                platforms!
               </p>
             </div>
           </div>
         </motion.div>
       </AnimatePresence>
+
       <div className="footerNote">
         Built and designed by Steven Klavins 2020
       </div>
