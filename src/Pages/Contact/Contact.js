0b0af687d8e2d9cd9b1ec546330e381817be4ffc
Steven <stevenklavins94@gmail.com>
2020-06-23 09:53:24 +0100

    chore: added animation presence

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -1,19 +1,23 @@
 import React from 'react'
 import '../Contact/Contact.css'
 import { motion } from 'framer-motion'
+import { AnimatePresence } from 'framer-motion'
 import ContactForm from '../../Components/ContactForm/ContactForm'
 
 const Contact = () => {
   return (
     <div>
-      <motion.div
-        exit={{ opasity: 0 }}
-        animate={{ opacity: 1 }}
-        initial={{ opacity: 0 }}
-      >
-        <h1 className="nameTitle">Contact</h1>
-        <ContactForm />
-      </motion.div>
+      <AnimatePresence onExitComplete>
+        <motion.div
+          key="contact"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <h1 className="nameTitle">Contact</h1>
+          <ContactForm />
+        </motion.div>
+      </AnimatePresence>
       <div className="footerNote">
         Built and designed by Steven Klavins 2020
       </div>
