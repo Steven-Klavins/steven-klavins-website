07477dd7162e6622901b130680e3061aa08cba20
Steven <stevenklavins94@gmail.com>
2020-06-23 09:53:11 +0100

    chore: added animation presence

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -2,23 +2,26 @@ import React from 'react'
 import '../About/About.css'
 import aboutPhoto from '../../images/about-photo.jpg'
 import { motion } from 'framer-motion'
-
+import { AnimatePresence } from 'framer-motion'
 function About() {
   return (
     <div>
-      <motion.div
-        exit={{ opasity: 0 }}
-        animate={{ opacity: 1 }}
-        initial={{ opacity: 0 }}
-      >
-        <h1 className="nameTitle">About</h1>
-        <div className="aboutTextbox">
-          <p className="aboutTextbox">
-            Hello and thankyou for visiting my site!
-          </p>
-        </div>
-        <img className="aboutimg" src={aboutPhoto} alt="About"></img>
-      </motion.div>
+      <AnimatePresence onExitComplete>
+        <motion.div
+          key="about"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+        >
+          <h1 className="nameTitle">About</h1>
+          <div className="aboutTextbox">
+            <p className="aboutTextbox">
+              Hello and thankyou for visiting my site!
+            </p>
+          </div>
+          <img className="aboutimg" src={aboutPhoto} alt="About"></img>
+        </motion.div>
+      </AnimatePresence>
       <div className="footerNote">
         Built and designed by Steven Klavins 2020
       </div>
