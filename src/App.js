60141b00f4e14cd3f766b7937c9a9b8372c46017
Steven <stevenklavins94@gmail.com>
2020-06-23 17:12:57 +0100

    chore: removed uneeded imports

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useLocation,
-} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import ReactDOM from 'react-dom'
 import { AnimatePresence } from 'framer-motion'
 import NavBar from './Components/NavBar/NavBar'
