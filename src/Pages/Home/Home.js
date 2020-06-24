0c842b678c70f87325d0299dce813b6939ac2377
Steven <stevenklavins94@gmail.com>
2020-06-24 11:16:42 +0100

    feat: added query string for custom welcome msg

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,46 +1,51 @@
-import React from 'react'
+import React, { Component } from 'react'
 import '../Home/Home.css'
 import { motion } from 'framer-motion'
 import { AnimatePresence } from 'framer-motion'
+import queryString from 'query-string'
 
-const Home = (isVisible) => {
-  return (
-    <div className="fixed">
-      <AnimatePresence onExitComplete>
-        <motion.div
-          key="home"
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          exit={{ opacity: 0 }}
-        >
-          <div>
-            <h1 className="nameTitle">Steven Klavins</h1>
+class Home extends Component {
+  render() {
+    const value = queryString.parse(this.props.location.search)
+    const name = value.name
+    return (
+      <div className="fixed">
+        <AnimatePresence onExitComplete>
+          <motion.div
+            key="home"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
             <div>
-              <h2>Welcome</h2>
-              <p>
-                Welcome and thank you for visiting my website! I'm a full-stack
-                developer, Makers Academy graduate, and tech addict. I'm London
-                based however I'm an experienced remote worker and have the
-                flexibility to relocate/travel.<br></br>
-                <br></br>I hold a BA in Digital Arts, perform in a band, and
-                have a love of all things creative. I yearn to make the world a
-                better place through innovative tech, to be frank, its what gets
-                me out of bed in the morning.  
-                <br />
-                <br />
-                Want to chat? Feel free to contact me on any of the listed
-                platforms!
-              </p>
+              <h1 className="nameTitle">Steven Klavins</h1>
+              <div>
+                <h2>Welcome {name} </h2>
+                <p>
+                  Thank you so much for visiting my website! I'm a full-stack
+                  developer, Makers Academy graduate, and tech addict. I'm
+                  London based however I'm an experienced remote worker and have
+                  the flexibility to relocate/travel.<br></br>
+                  <br></br>I hold a BA in Digital Arts, perform in a band, and
+                  have a love of all things creative. I yearn to make the world
+                  a better place through innovative tech, to be frank, its what
+                  gets me out of bed in the morning.  
+                  <br />
+                  <br />
+                  Want to chat? Feel free to contact me on any of the listed
+                  platforms!
+                </p>
+              </div>
             </div>
-          </div>
-        </motion.div>
-      </AnimatePresence>
+          </motion.div>
+        </AnimatePresence>
 
-      <div className="footerNote">
-        Built and designed by Steven Klavins 2020
+        <div className="footerNote">
+          Built and designed by Steven Klavins 2020
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Home
