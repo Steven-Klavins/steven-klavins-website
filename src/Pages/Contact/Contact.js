99a36472fe4a46e133bd0f80300349f6640d54da
Steven <stevenklavins94@gmail.com>
2020-06-24 11:17:20 +0100

    chore: updated contact info

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -14,6 +14,10 @@ const Contact = () => {
           exit={{ opacity: 0 }}
         >
           <h1 className="nameTitle">Contact</h1>
+          <p>
+            stevenklavins@gmail94.com <br></br>
+            The best way to contact me is via email or any of my social
+          </p>
           {/*import ContactForm from '../../Components/ContactForm/ContactForm' <ContactForm />*/}
         </motion.div>
       </AnimatePresence>
