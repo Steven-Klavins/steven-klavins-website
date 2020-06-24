86677f3d789b153510cc59c9e7a6cf9170b53588
Steven <stevenklavins94@gmail.com>
2020-06-24 13:36:34 +0100

    Feat: added animation to contact

--- src/Pages/Contact/Contact.js
+++ src/Pages/Contact/Contact.js
@@ -2,6 +2,35 @@ import React from 'react'
 import '../Contact/Contact.css'
 import { motion } from 'framer-motion'
 import { AnimatePresence } from 'framer-motion'
+import gmailLogo from '../../images/gmail-logo.png'
+
+const container = {
+  start: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+  end: {
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+}
+
+const circlesAnimation = {
+  start: {
+    y: '0%',
+  },
+  end: {
+    y: '100%',
+  },
+}
+
+const bounce = {
+  duration: 0.4,
+  yoyo: Infinity,
+  ease: 'easeInOut',
+}
 
 const Contact = () => {
   return (
@@ -18,7 +47,32 @@ const Contact = () => {
             stevenklavins@gmail94.com <br></br>
             The best way to contact me is via email or any of my social
           </p>
+          <img className="mailLogo" src={gmailLogo} alt="Mail Logo"></img>
           {/*import ContactForm from '../../Components/ContactForm/ContactForm' <ContactForm />*/}
+          <div className="animatePosition">
+            <motion.div
+              className="bounce"
+              variants={container}
+              initial="start"
+              animate="end"
+            >
+              <motion.span
+                className="circle"
+                variants={circlesAnimation}
+                transition={bounce}
+              />
+              <motion.span
+                className="circle"
+                variants={circlesAnimation}
+                transition={bounce}
+              />
+              <motion.span
+                className="circle"
+                variants={circlesAnimation}
+                transition={bounce}
+              />
+            </motion.div>
+          </div>
         </motion.div>
       </AnimatePresence>
       <div className="footerNote">
