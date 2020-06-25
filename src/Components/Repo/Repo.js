e94f189c7d929af87b59133af5df4b24c35556ee
Steven <stevenklavins94@gmail.com>
2020-06-25 01:24:14 +0100

    chore: fixed link react warning

--- src/Components/Repo/Repo.js
+++ src/Components/Repo/Repo.js
@@ -5,21 +5,26 @@ import gitHubIcon from '../../images/github-logo.png'
 export class Repo extends React.Component {
   render() {
     return (
-      <div className="repBox">
+      <div className="repocontainer">
         <span className="repoTitle">{this.props.name}</span>
         <div className="gitHubIcon">
           <img className="gitHubIcon" src={gitHubIcon} alt="GitHub Icon"></img>
-          <a className="ghLink" href={this.props.link} target="_blank">
+          <a
+            className="githublink"
+            href={this.props.link}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             Link
           </a>
         </div>
-        <span className="pn">{this.props.description}</span>
+        <span className="repoinfotext">{this.props.description}</span>
         <br></br>
-        <span className="pn">
+        <span className="repoinfotext">
           Last updated: {this.props.update.slice(0, 10)}{' '}
           {this.props.update.slice(11, 16)}
         </span>
-        <span className="pn">Language: {this.props.language}</span>
+        <span className="repoinfotext">Language: {this.props.language}</span>
       </div>
     )
   }
